62458dc02bb2dc9c965e3b2572ef06f845da6c37
Romain Ballesteros <romain.ballesteros@gmail.com>
2023-06-06 10:31:24 +0200

    add pages in folder page

--- php/header.php
+++ php/header.php
@@ -18,7 +18,7 @@
         <a href="../index.html"><img class="logo" src="../image/Garage_V.parrot-removebg-preview.png"
                 alt="logo garage"></a>
         <div class="navbar">
-            <a href="./connexion.html"><button class="connexion">connexion</button></a>
+            <a href="../connexion.html"><button class="connexion">connexion</button></a>
             <nav class="Le_menu">
                 <div class="toggle">
                     <img src="../image/bars-solid.svg" alt="menu button" class="ouvrir">
@@ -34,30 +34,3 @@
             </nav>
         </div>
     </header>
-    <h2>connexion</h2>
-    <div class="form-i">
-        <form class="inscription">
-            <label for="email">email:</label><br>
-            <input type="email" name="email" id="email"><br>
-            <label for="password">Mot de passe</label><br>
-            <input type="password" name="password" id="password">
-        </form>
-    </div>
-    <footer>
-        <div class="bg-blk footer">
-            <p>Nos horaires: <br>
-                du mardi au vendredi <br>
-                de 9h à 12h <br> et de <br> 13h à 18h <br>samedi <br>de 10h à 15h</p>
-            <div class="trait2"></div>
-            <img src="../image/Garage_V.parrot-removebg-preview.png" alt="logo">
-            <p>contactez nous : <br>01 85 66 32 15 <br>contact@vparrot.fr <br>30 rue de la reussite <br>34000
-                <br>montpellier
-            </p>
-        </div>
-    </footer>
-
-    <script src="../script.js"></script>
-
-</body>
-
-</html>
